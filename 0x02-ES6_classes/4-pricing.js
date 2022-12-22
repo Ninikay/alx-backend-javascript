a47e1c7e7b11ea91159bb28daa3c020a11d160e4
Ninikay <ms.nikeoni@gmail.com>
2022-12-22 22:16:45 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,34 +1,69 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
+/**
+ * Represents pricing class
+ */
 export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
   }
 
-  // amount
+  /**
+   * Set amount of pricing
+   */
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  /**
+   * Get amount of pricing
+   */
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
+  /**
+   * Set currency of pricing
+   */
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency object');
+    }
+    this._currency = currency;
   }
 
-  // currency
+  /**
+   * Get currency of the pricing
+   */
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
-  }
-
+  /**
+   * displayFullPrice - display full price
+   * @returns {String}
+   */
   displayFullPrice() {
-    return (`${this._amount} ${this.currency.name} (${this.currency.code})`);
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
+  /**
+   * convertPrice - convert price
+   * @param {Number} amount - amount of price
+   * @param {Number} conversionRate - conversion rate
+   * @returns
+   */
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('ConversionRate must be a number');
+    }
     return amount * conversionRate;
   }
 }
